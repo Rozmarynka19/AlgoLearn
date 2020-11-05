aed2f8db8144c13c1ba7d8ec786e8157e8c13a46
mo44415 <mo44415@zut.edu.pl>
2020-11-05 11:04:39 +0100

    Calculate progress, keep track of done category, dynamic update text and css style

--- AlgoLearn/src/algolearn/gui/FXMLDocumentController.java
+++ AlgoLearn/src/algolearn/gui/FXMLDocumentController.java
@@ -22,42 +22,75 @@ public class FXMLDocumentController implements Initializable {
 	public double [] scene_max = {300, 1105};
 	private boolean resize_locker = false;
     @FXML
-    private Button btn;
+    private Button btn, wpr, opi, wiz;;
     private String btn_id = "NULL";
+    private boolean[][] category_data =  {
+    		{false, false, false},
+    		{false, false, false},
+    		{false, false, false},
+    		{false, false, false},
+    		{false, false, false},
+    		{false, false, false},
+    		{false, false, false},
+    		{false, false, false}
+    };
+    private int algo_id = 0;
     @FXML
-    private Text txtMainTitle;
-    private String txtMainTitleString = "Algolearn - ";
+    private Text txtMainTitle, txtProgress;
+    private String txtMainTitleString = "Algolearn - ", txtProgressString = "/100";
     @FXML /* Expand window */
     private void handleButtonAction(ActionEvent event) throws Exception {
         Stage stage = (Stage) btn.getScene().getWindow();
         Button clicked_btn = (Button)event.getSource();
         String btn_val = clicked_btn.getId();
+        this.algo_id = Integer.parseInt(btn_val);
         if(this.btn_id == "NULL") this.btn_id = btn_val;
-        if(stage.getWidth() < scene_max[1] && this.resize_locker == false && clicked_btn.getId() == btn_id) {
+        if(stage.getWidth() < scene_max[1] && this.resize_locker == false) {
         	resize(stage, scene_max[1], (double)15, 1, 3);
         	txtMainTitle.setText(txtMainTitleString + clicked_btn.getText());
+        	categorySetBackground();
+        	setProgress(calculateProgress());
         }
         else if (stage.getWidth() >= scene_max[1] && this.resize_locker == false && clicked_btn.getId() == btn_id)  {
         	resize(stage, scene_base[1], (double)-15, 1, 3);
         }else if(clicked_btn.getId() != btn_id) {
         	txtMainTitle.setText(txtMainTitleString + clicked_btn.getText());
+        	categorySetBackground();
+        	setProgress(calculateProgress());
         }
         this.btn_id = clicked_btn.getId();
     }
     
     @FXML
     private ProgressBar progressBar;
-    
-    @FXML
-    private Button wpr, opi, wiz;
 
+    @FXML
+    private void categorySetBackground(){
+    	wpr.getStyleClass().clear();
+    	wiz.getStyleClass().clear();
+    	opi.getStyleClass().clear();
+    	
+    	if(category_data[this.algo_id][0] == false)
+    		wpr.getStyleClass().add("wprowadzenie");
+    	else if(category_data[this.algo_id][0] == true)
+    		wpr.getStyleClass().add("wprowadzenie2");
+    	
+    	if(category_data[this.algo_id][1] == false)
+    		opi.getStyleClass().add("opis");
+    	else if(category_data[this.algo_id][1] == true)
+    		opi.getStyleClass().add("opis2");
+    	
+    	if(category_data[this.algo_id][2] == false)
+    		wiz.getStyleClass().add("wizualizacja");
+    	else if(category_data[this.algo_id][2] == true)
+    		wiz.getStyleClass().add("wizualizacja2");	
+    }
+    
     @FXML /* Expand window */
     private void handleCloseWindowAction(ActionEvent event) throws Exception {
     	Stage stage = (Stage) btn.getScene().getWindow();
     	stage.close();
     }
-    
-    private long time_exec = 0;
     
     /* Resize window */
     private void resize(Stage stage, double width, double speed, int delay, int wait) {
@@ -79,19 +112,41 @@ public class FXMLDocumentController implements Initializable {
             }
 
         }, delay, wait);
-        time_exec = System.nanoTime();
     	this.resize_locker = false;
     }
 
     public void handleProgress(ActionEvent event) {
     	Button clicked_btn = (Button)event.getSource();
     	String id = clicked_btn.getId();
-    	if(id.equals(wpr.getId()))
-    			setProgress(0.33);
-    	else if(id.equals(opi.getId()))
-			setProgress(0.66);
-    	else if(id.equals(wiz.getId()))
-			setProgress((double)1);
+    	
+    	if(id.equals(wpr.getId())) 
+    		category_data[this.algo_id][0] = category_data[this.algo_id][0] ? false : true;
+    	else if(id.equals(opi.getId())) 
+    		category_data[this.algo_id][1] = category_data[this.algo_id][1] ? false : true;
+    	else if(id.equals(wiz.getId())) 
+    		category_data[this.algo_id][2] = category_data[this.algo_id][2] ? false : true;
+    	
+    	setProgress(calculateProgress());
+    	int prog = (int)(calculateProgress() * 100);
+    	categorySetBackground();
+    	txtProgress.setText(Integer.toString(prog)+txtProgressString);
+    }
+    
+    private double calculateProgress() {
+    	int count_done = 0;
+    	for(int i = 0 ; i < 3; i++)
+    		if(category_data[this.algo_id][i] == true) 
+    			count_done++;
+    	switch(count_done) {
+    		case 1:
+    			return 0.33;
+    		case 2:
+    			return 0.66;
+    		case 3:
+    			return 1.0;
+    		default:
+    			return 0;
+    	}
     }
     
     private void setProgress(double progress) {
