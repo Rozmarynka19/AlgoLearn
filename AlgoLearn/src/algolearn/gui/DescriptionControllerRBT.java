7062980c961aff4031f6f19da68bc38cfe91b427
Monika Rozmarynowska <morozmarynowska@gmail.com>
2021-01-06 22:47:56 +0100

    Add lang button on click method - RBT description

--- AlgoLearn/src/algolearn/gui/DescriptionControllerRBT.java
+++ AlgoLearn/src/algolearn/gui/DescriptionControllerRBT.java
@@ -30,47 +30,75 @@ public class DescriptionControllerRBT extends FXMLDocumentController implements 
     @FXML
     WebView introText;
     private WebEngine engine;
+    
+    @FXML
+    Button cppButton;
+    @FXML
+    Button javaButton;
+    @FXML
+    Button pyButton;
+    
+    private String selectedLangButton; 
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		engine = introText.getEngine();
+//		engine = introText.getEngine();
+	}
+	
+	
+	@FXML
+	public void onLangButtonClickAction(ActionEvent event) throws IOException {
+		//set styles of all buttons to the default
+		cppButton.getStyleClass().remove("langBTNselected");
+		cppButton.getStyleClass().add("langBTN");
+		javaButton.getStyleClass().clear();
+		javaButton.getStyleClass().add("langBTN");
+		pyButton.getStyleClass().clear();
+		pyButton.getStyleClass().add("langBTN");
+		//set specified style to selected button
+		Button clickedButton = (Button)event.getSource();
+		clickedButton.getStyleClass().clear();
+		clickedButton.getStyleClass().add("langBTNselected");
+		//save in any variable which button was selected
+		selectedLangButton = clickedButton.getText();
+		System.out.println("Button selected: "+selectedLangButton);
 	}
 
-    @FXML
-    public void loadText(ActionEvent event) throws IOException {
-        Button clicked_btn = (Button)event.getSource();
-        Path path;
-        final String script;
-        path= Paths.get("src/algolearn/gui/Html/RBT.html");
-        if(clicked_btn.getText().equals("Wstawianie Węzła")){
-            script="test1()";
-        }
-        else if(clicked_btn.getText().equals("Rotacje")){
-            script="test2()";
-        }
-        else if(clicked_btn.getText().equals("Wyszukiwanie Węzła")){
-            script="test3()";
-        }
-        else if(clicked_btn.getText().equals("Wprowadzenie")){
-            script="test4()";
-        }
-        else {
-            script = "test5()";
-        }
-        engine = introText.getEngine();
-        engine.setJavaScriptEnabled(true);
-        engine.load( "file:///"+path.toAbsolutePath());
-        engine.getLoadWorker().stateProperty().addListener(
-                new ChangeListener() {
-                    @Override
-                    public void changed(ObservableValue observable, Object oldValue, Object newValue) {
-                        if (newValue != Worker.State.SUCCEEDED) {
-                            return;
-                        }
-                        String hello2 = (String) engine.executeScript("renev()");
-                        String hello = (String) engine.executeScript(script);
-                    }
-                }
-        );
-    }
+//    @FXML
+//    public void loadText(ActionEvent event) throws IOException {
+//        Button clicked_btn = (Button)event.getSource();
+//        Path path;
+//        final String script;
+//        path= Paths.get("src/algolearn/gui/Html/RBT.html");
+//        if(clicked_btn.getText().equals("Wstawianie Węzła")){
+//            script="test1()";
+//        }
+//        else if(clicked_btn.getText().equals("Rotacje")){
+//            script="test2()";
+//        }
+//        else if(clicked_btn.getText().equals("Wyszukiwanie Węzła")){
+//            script="test3()";
+//        }
+//        else if(clicked_btn.getText().equals("Wprowadzenie")){
+//            script="test4()";
+//        }
+//        else {
+//            script = "test5()";
+//        }
+//        engine = introText.getEngine();
+//        engine.setJavaScriptEnabled(true);
+//        engine.load( "file:///"+path.toAbsolutePath());
+//        engine.getLoadWorker().stateProperty().addListener(
+//                new ChangeListener() {
+//                    @Override
+//                    public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+//                        if (newValue != Worker.State.SUCCEEDED) {
+//                            return;
+//                        }
+//                        String hello2 = (String) engine.executeScript("renev()");
+//                        String hello = (String) engine.executeScript(script);
+//                    }
+//                }
+//        );
+//    }
 }
