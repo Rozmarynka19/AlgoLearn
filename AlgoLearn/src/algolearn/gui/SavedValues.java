80a70de219baf9a8e29bd3dfae10a32c444eee17
Monika Rozmarynowska <morozmarynowska@gmail.com>
2021-01-20 20:21:24 +0100

    [SkipList] Bind codes

--- AlgoLearn/src/algolearn/gui/SavedValues.java
+++ AlgoLearn/src/algolearn/gui/SavedValues.java
@@ -20,7 +20,7 @@ public class SavedValues {
             asList("countingSort_introduction.fxml","countingSort_description.fxml","visualisation_fxml.fxml"), // Sortowanie przez zliczanie
             asList("heapSort_introduction.fxml","heapSort_description.fxml","heap_sort_visualisation.fxml"), // Sortowanie przez kopcowanie
             asList("FFT_introduction.fxml","FFT_description.fxml","visualisation_fxml.fxml"), // fft
-            asList("skip_list_introduction.fxml","description_fxml.fxml","skip_list_visualisation.fxml"), // lista z przeskokami
+            asList("skip_list_introduction.fxml","skip_list_description.fxml","skip_list_visualisation.fxml"), // lista z przeskokami
             asList("BST_introduction.fxml","BST_description.fxml","bst_visualisation.fxml"), // bst
             asList("hashTable_introduction.fxml","hashTable_description.fxml","hash_table_visualisation.fxml"), // tablica mieszająca
             asList("FloydWarshall_introduction.fxml","FloydWarshall_description.fxml","visualisation_fxml.fxml"), // floyd warshall
