c46feb6a705c671f25e010523f6276a3d3b695d7
mo44415 <mo44415@zut.edu.pl>
2021-01-26 01:30:45 +0100

    test-heap

--- AlgoLearn/src/algolearn/gui/testsData.java
+++ AlgoLearn/src/algolearn/gui/testsData.java
@@ -17,7 +17,7 @@ public class testsData {
     
     // Ilosc pytan w testach
     protected static int questionsCount[] = {
-    		2,
+    		5,
     		10,
     		10,
     		10,
@@ -37,13 +37,19 @@ public class testsData {
     		{	
     			{true, false, false, false},
     			{false, false, false, true},
+    			{true, false, false, true},
+    			{true, false, false, false},
+    			{false, false, true, false},
     		},
     };
     
     protected static String [][] questionsHTML = {
     		{	
-    			"tests/binary_heap/zadanie1.html",
-    			"html/RBT.html",
+    			"tests/binary_heap/question1.html",
+    			"tests/binary_heap/question2.html",
+    			"tests/binary_heap/question3.html",
+    			"tests/binary_heap/question4.html",
+    			"tests/binary_heap/question5.html",
     		}
     };
 }
